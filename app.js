afbddb28e255db7283aef6c735d0639ff06d83c6
Harshad.N <harshad.n4321@gmail.com>
2023-03-01 23:29:52 +0530

    auth set setup is done

--- app.js
+++ app.js
@@ -8,17 +8,21 @@ const { auth } = require('express-openid-connect');
 const config = {
     authRequired: false,
     auth0Logout: true,
-    secret: proccess.env.SECRET,
-    baseURL: proccess.env.BASE_URL,
-    clientID: proccess.env.CLIENT_ID,
-    issuerBaseURL: proccess.env.ISSUER_BASE_URL
-};
-
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+}
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
-const port = proces.env.PORT || 3000
+
+
+const port = process.env.PORT || 3000
 
 
 app.listen(port, () => (
