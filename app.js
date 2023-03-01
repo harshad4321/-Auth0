5c0d4c23fbcacc0b118e501cc28ffc04171857b7
Harshad.N <harshad.n4321@gmail.com>
2023-03-01 23:38:58 +0530

    It is quite easy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 require('dotenv').config();
 
 
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const config = {
     authRequired: false,
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user))
+})
 
 
 const port = process.env.PORT || 3000
