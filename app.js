f75399ed90da03097a8f70560e3236ed7ebbe8dd
Harshad.N <harshad.n4321@gmail.com>
2023-03-01 23:22:26 +0530

    env

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
 
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: proccess.env.SECRET,
+    baseURL: proccess.env.BASE_URL,
+    clientID: proccess.env.CLIENT_ID,
+    issuerBaseURL: proccess.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 
 const port = proces.env.PORT || 3000
